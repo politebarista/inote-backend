3e8a25d1bd1fc84f631e28bc6484a10ae2452bbb
Ilya Sulimenko <twid01gc@gmail.com>
2021-06-06 15:35:22 +0700

    transmission of notes on demand has been implemented

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pymysql
 
 app = Flask(__name__)
 
+connection = pymysql.connect(host="localhost",
+                             user="root",
+                             password="root",
+                             database="inote",
+                             cursorclass=pymysql.cursors.DictCursor,
+                             )
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'welcome)'
+
+
+@app.route('/getNotes', methods=['GET'])
+def get_notes():
+    cursos = connection.cursor()
+    query = "select * from notes"
+    cursos.execute(query)
+    result = cursos.fetchall()
+    print('received ' + str(len(result)) + ' notes')
+    jsonedResult = jsonify(result)
+
+    return jsonedResult
 
 
 if __name__ == '__main__':
